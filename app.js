195f2072b47f8a52582581c58e574784f1bab413
shalom belaineh <41238172+shalina2@users.noreply.github.com>
2018-09-12 08:11:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var code= prompt("you think i like code 201?");
+
+if (code.toLowerCase() === "yes") {
+     console.log('you are very smart')
+ } else{ 
+     console.log('NO, I love code 201');
+     
+ }
+ var age= prompt('Do i look like 27 years old?')
+ if (age.toLowerCase() === "yes") {
+    console.log('Am glad i look like my age');
+} else if (age.toLowerCase()==="no") {
+    console.log('you missed that am 27 years old')
+} var color=prompt('what is my favorite color?')
+
+if (color === "red") {
+    console.log('that is my favorite color');
+} else {
+    console.log('that is not my favorite color')};
