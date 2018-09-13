9b2e5cc97387db5835f1d2bf7bc29924f6b254f5
shalom belaineh <shalom.belaineh@gmail.com>
2018-09-13 02:21:33 -0700

    intial commit of index.html

---
+++ app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var code = prompt("you think i like code 201?");
+
+if (code.toLowerCase() === "yes") {
+     console.log('you are very smart');
+     alert('you are very smart');
+ } else{ 
+     console.log('I do love code 201');
+     alert('I do love code 201');
+     
+ }
+ var fun = prompt('do you think i like hiking?')
+ if (fun.toLowerCase() === "yes") {
+    console.log('you got it');
+    alert('you got it')
+} else  {
+    console.log('you better get to know me,I like to go hiking');
+    alert('you better get to know me,I like to go hiking')
+} var color = prompt('you think red is my favorite color?')
+
+if (color === "yes") {
+    console.log('you/re right it is my favorite color');
+    alert('you/re right it is my favorite color');
+} else {
+    console.log('it is my favorite color')
+    alert('it is my favorite color')};
+    var height = prompt('you think i am 5/7 tall');
+    if (height==="yes"){
+        console.log('Amazing,thats right')
+        alert('Amazing,thats right');
+    } else {
+        console.log('sadly,you missed that');
+        alert('sadly,you missed that');
+    } var drink = prompt('you think i like tea more than coffee?');{
+        if(drink.toLowerCase()==="yes"){
+            console.log('thats right,i like tea');
+            alert('thats right,i like tea')
+        }else console.log('not really,i prefer tea more')
+                alert('not really,i prefer tea more');
+    } 
+    var love =prompt('What do you think I love Most?');
+    var answer =[ethiopia,dancing,eating,swimming,hiking,sleeping]
+    var arraylength =answer.length;
+    var roundnumber=0;
+    var i;
+        for (i = 0; i< arraylength;i++)
+            roundnumber = (i+ 1);{
+                console.log('you got it')
+            
+            alert('you got it');
+        } for (i = 0; i< arraylength;i++)
+        roundnumber = (i+ 2);{
+            console.log('I dont mind it')
+        alert('I dont mind it')}
+        var number =prompt('How long do u think i moved to the states?');
+        var answer = [1,3,5,7,9,0];
+        for (i=0;i< answer.length;i++);{
+            console.log('you missed it,but try again')
+            alert('you missed it,but try again')}
+        for (i=1;i< answer.length;i++);{
+            console.log('yay,you are right')
+            alert('yay,you are right')}
+        
+        
+        
+    
